6bad1a1ce5e4d3a12e29e1834059c69cd4029861
allseenn <allseen@live.ru>
2024-11-10 10:52:30 +0000

    05

--- 05.Lecture/app/src/App.js
+++ 05.Lecture/app/src/App.js
@@ -1,25 +1,30 @@
+// import { createContext, useContext, useState} from 'react'
+
+// Как было раньше
+function GrandParent() {
+  return <Parent message="Hello from top" />
+};
+
+
+function Parent({props}) {
+  return <Child message={props.message} />
+};
+
+
+function Child({props}) {
+  return <GrandChild message={props.message} />
+};
+
+function GrandGrandGrandChild({props}) {
+  return <span>{props.message}</span>
+};
+
+// Создаем контекст
+
 function App() {
   return (
-    <>
-      <GrandParent />
-    </>
+    <GrandParent />
   );
 }
 
-function GrandParent() {
-  return <Parent message="Hello from top" />
-}
-
-function Parent(props) {
-  return <Child message={props.message} />
-}
-
-function Child(props) {
-  return <GrandChild message={props.message} />
-}
-
-function GrandChild(props) {
-  return <span>{props.message}</span>
-}
-
-export default App;
+export default App
